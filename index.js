a2224a5493fe13a8139c3bbc35313a815879d462
Faithlucy <cecygachanja5@gmail.com>
2023-09-27 18:23:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1=31 
+const num2=2
+const multiply= num1 * num2
+
+const random= 1
+Math.floor(Math.random() * 100) +1;
+
+const num3=29
+const num4=5
+const mod=num3%num4
+
+const max= Math.max(16,17,18,19,20)
